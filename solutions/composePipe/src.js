8d42c956b617e581a14f09f4a790452be4359793
Tre Ammatuna <tre@tretuna.com>
2017-05-13 22:10:01 -0400

    composePipe finished

--- solutions/composePipe/src.js
+++ solutions/composePipe/src.js
@@ -1,7 +1,15 @@
-const compose = () => {
-
+const compose = (...args) => {
+  return (val) => {
+    return args.reduceRight((memo, fn) => {
+      return fn(memo);
+    }, val);
+  };
 };
 
-const pipe = () => {
-
+const pipe = (...args) => {
+  return (val) => {
+    return args.reduce((memo, fn) => {
+      return fn(memo);
+    }, val);
+  };
 };
