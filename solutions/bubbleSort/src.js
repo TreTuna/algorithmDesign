f1c7e82c5a09c4b161fab01cbb05c8afc40d8f52
Tre Ammatuna <tre@tretuna.com>
2017-05-13 21:13:50 -0400

    Final tweak of Bubble Sort

--- solutions/bubbleSort/src.js
+++ solutions/bubbleSort/src.js
@@ -1,9 +1,16 @@
+const swap = (a, b, array) => {
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+  return array;
+};
+
 const bubbleSort = (array) => {
   while (true){
     let swaps = false;
-    for(let i = 0; i < array.length - 1; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
       let next = i + 1;
-      if (array[i] > array[next]){
+      if (array[i] > array[next]) {
         swaps = true;
         swap(i, next, array);
       }
@@ -15,15 +22,6 @@ const bubbleSort = (array) => {
   return array;
 };
 
-const swap = (a, b, array) => {
-  let temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
-  return array;
-}
-
-let testArray = [3, 6, 2, 5, 1, -5];
-
-
+const testArray = [3, 6, 2, 5, 1, -5];
 
 console.log(bubbleSort(testArray));
