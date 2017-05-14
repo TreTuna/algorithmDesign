c885267f6f695e29ac24fd575f9be7d0a5361b17
Tre Ammatuna <tre@tretuna.com>
2017-05-13 23:40:50 -0400

    Finished naive solution to commonCharacters, refactor to come

--- solutions/commonCharacters/spec.js
+++ solutions/commonCharacters/spec.js
@@ -1,14 +1,27 @@
 var should = chai.should();
 
-describe('set', function() {
-  var set;
-
-  beforeEach(function() {
-    // Something do to before each test
+describe('Common Characters', function(){
+  it('returns common characters', function(){
+    commonCharacters('abc', 'abc').should.equal('abc');
   });
 
-  it('should pass a basic test', function() {
-    expect(1).to.equal(1);
+  it('returns common characters', function(){
+    commonCharacters("What is love?", "Baby don't hurt me").should.equal('hatoe');
   });
 
+  it('returns common characters', function(){
+    commonCharacters('Riding on a buffalo makes me more approachable', 'so what').should.equal('oash');
+  });
+
+  it('returns empty string', function(){
+    commonCharacters('', 'No more').should.equal('');
+  });
+
+  it('returns empty string', function(){
+    commonCharacters('No more', '').should.equal('');
+  });
+
+  it('returns empty string', function(){
+    commonCharacters('', '').should.equal('')
+  });
 });
--- solutions/commonCharacters/src.js
+++ solutions/commonCharacters/src.js
@@ -1,3 +1,16 @@
-var template = function(){
-  return template;
+commonCharacters = function(string1, string2){
+  if(string1 === '' || string2 === ''){
+    return '';
+  }
+
+  var results = [];
+
+  for(var i = 0; i < string1.length; i++){
+    var currentChar = string1[i]
+    if(string2.includes(currentChar) && !results.includes(currentChar) && currentChar !== ' '){
+      results.push(currentChar);
+    }
+  }
+
+  return results.join('');
 };
