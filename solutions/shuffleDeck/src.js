f5827084dae56272c4baec02f568231ccb9175fd
Tre Ammatuna <tre@tretuna.com>
2017-05-19 23:37:01 -0400

    Cleaned up shuffleDeck code

--- solutions/shuffleDeck/src.js
+++ solutions/shuffleDeck/src.js
@@ -6,18 +6,12 @@ const swap = (a, b, array) => {
 };
 
 const shuffleDeck = function(deck) {
-  // we shuffle into the front of the array
-  // i is the first index of the un-shuffled portion
   for (var i = 0; i < deck.length; i++) {
-    // select a random card from the un-shuffled portion
     var pick = i + Math.floor(Math.random() * (deck.length - i));
-    // swap that card with the first un-shuffled card
     swap(i, pick, deck);
-    // since the pick is selected randomly each time, the swap is not biasing
   }
   return deck;
 };
-
 
 // var orderedDeck = function() {
 //   var suits = [ '♥', '♣', '♠', '♦' ];
